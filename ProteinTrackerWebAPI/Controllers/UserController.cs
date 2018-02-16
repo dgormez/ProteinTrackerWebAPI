cbfc940a94d10b81eae019df3c1be63111f86b1f
dgormez <dgormez@gmail.com>
2018-02-16 13:38:26 +0100

    Web API & webservices

---
+++ ProteinTrackerWebAPI/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProteinTrackerWebAPI.Models;
+
+namespace ProteinTrackerWebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        private UserRepository repository = new UserRepository();
+
+        //api/user/{id}
+        public int Put(int id, [FromBody] int amount)
+        {
+            var user = repository.GetById(id);
+            if (user == null)
+                return -1;
+            user.Total += amount;
+            repository.Save(user);
+            return user.Total;
+        }
+
+        public int Post([FromBody]CreateUserRequest request)
+        {
+            var user = new User { Goal = request.goal, Name = request.name, Total = 0 };
+            repository.Add(user);
+
+            return user.UserId;
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return new List<User>(repository.GetAll());
+        }
+
+        public class CreateUserRequest
+        {
+            public string name { get; set; }
+            public int goal { get; set; }
+        }
+    }
+}
---
+++ ProteinTrackerWebAPI/Models/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace ProteinTrackerWebAPI.Models
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        ReadOnlyCollection<User> GetAll();
+        User GetById(int id);
+        void Save(User updatedUser);
+    }
+}
